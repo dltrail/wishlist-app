3e7cf45b28b857ed0e56e11b1ba58b971c5c25a8
Dawn <dtrail76@gmail.com>
2023-09-08 16:40:25 +0100

    serve front folder

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const { errorHandler } = require("./middleware/errorMiddleware");
@@ -16,6 +18,20 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/wishlist", require("./routes/wishlistRoutes.js"));
 app.use("/api/users", require("./routes/userRoutes.js"));
+
+// Serve frontend
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
