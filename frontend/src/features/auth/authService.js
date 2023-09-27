e431b1853b122af5d97decf3cd939a94c638a7dd
Dawn Trail <dtrail76@gmail.com>
2023-09-27 22:23:52 +0100

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://wishlist-app-backend-q64vlczjv-dltrail.vercel.app/api/users/";
+const API_URL = "https://wishlist-app-backend.vercel.app/api/users/";
 
 // Register user
 const register = async (userData) => {
